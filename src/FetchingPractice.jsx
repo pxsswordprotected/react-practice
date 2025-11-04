05b199b92398186f2f7ec06d287ab2e9bc5d7f94
james <jamesalford661@gmail.com>
2025-11-04 17:59:26 -0600

    more fetching practice

--- src/FetchingPractice.jsx
+++ src/FetchingPractice.jsx
@@ -2,23 +2,34 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 const FetchingPractice = () => {
-  const [notes, setNotes] = useState([]);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  useEffect =
-    (() => {
-      axios.get("https://api.example.com/notes").then((promise) => {
-        setNotes(promise.data.notes);
+  useEffect(() => {
+    axios
+      .get("https://api.example.com/user/1")
+      .then((data) => {
+        console.log("data arrived:", data);
+        setUser(data.user);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log("there was an error:", err);
+        setError(err.message);
+        setLoading(false);
       });
-    },
-    []);
-};
+  }, []);
 
-return (
-  <ul>
-    {notes.map((note) => {
-      <li key={note}>{note}</li>;
-    })}
-  </ul>
-);
+  if (loading) return <p>Loading...</p>;
+  if (err) return <p>Error: {error}</p>;
+
+  return (
+    <>
+      <h1>{user.name}</h1>
+      <p>{user.email}</p>
+    </>
+  );
+};
 
 export default FetchingPractice;
