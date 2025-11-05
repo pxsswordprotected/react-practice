8875afdc3589643f0d0bcdf2166b71b9007ea89b
james <jamesalford661@gmail.com>
2025-11-04 18:41:58 -0600

    end of day practice

--- src/FetchingPractice.jsx
+++ src/FetchingPractice.jsx
@@ -1,35 +1,16 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const FetchingPractice = () => {
+const FetchingPractice = ({ userID }) => {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios
-      .get("https://api.example.com/user/1")
-      .then((data) => {
-        console.log("data arrived:", data);
-        setUser(data.user);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.log("there was an error:", err);
-        setError(err.message);
-        setLoading(false);
-      });
-  }, []);
+    axios.get(`https://api.example.com/user/${userId}`).then((r) => {
+      setUser(r.data);
+    });
+  }, [userID]);
 
-  if (loading) return <p>Loading...</p>;
-  if (err) return <p>Error: {error}</p>;
-
-  return (
-    <>
-      <h1>{user.name}</h1>
-      <p>{user.email}</p>
-    </>
-  );
+  return <></>;
 };
 
 export default FetchingPractice;
